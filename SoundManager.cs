5d2c8c6909f4f1796ff2f4f9d63f9fcf7c922c83
jiangbo <jiangb2o@foxmail.com>
2024-04-14 23:18:55 +0800

    Play Sounds Simultaneously

--- SoundManager.cs
+++ SoundManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Media;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,12 @@ namespace TankGame
 {
     enum SoundType
     {
-
+        Start,
+        Add,
+        Blast,
+        Fire,
+        Hit,
+        Count
     }
 
     class SoundManager
@@ -20,51 +26,67 @@ namespace TankGame
         //private static SoundPlayer blastPlayer = new SoundPlayer();
         //private static SoundPlayer firePlayer = new SoundPlayer();
         //private static SoundPlayer hitPlayer = new SoundPlayer();
-        
+ 
+        private static String dirPath = @"E:\CS\tank\TankGame\asset\Sounds";
+        private static String [] filePath = new String[(int)SoundType.Count];
+        private static String[] aliases = new String[(int)SoundType.Count];
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendString", CharSet = CharSet.Unicode)]
+        extern static ulong MciSendString(string command, string buffer, int bufferSize, IntPtr callback);
 
         public static void InitSound()
         {
-            //startPlayer.Stream = Properties.Resources.start;
-            //addPlayer.Stream = Properties.Resources.add;
-            //blastPlayer.Stream = Properties.Resources.blast;
-            //firePlayer.Stream = Properties.Resources.fire;
-            //hitPlayer.Stream = Properties.Resources.hit;
+            filePath[(int)SoundType.Start] = dirPath + @"\start.wav";
+            filePath[(int)SoundType.Add] = dirPath + @"\add.wav";
+            filePath[(int)SoundType.Blast] = dirPath + @"\blast.wav";
+            filePath[(int)SoundType.Fire] = dirPath + @"\fire.wav";
+            filePath[(int)SoundType.Hit] = dirPath + @"\hit.wav";
 
+            aliases[(int)SoundType.Start] = "start";
+            aliases[(int)SoundType.Add] = "add";
+            aliases[(int)SoundType.Blast] = "blast";
+            aliases[(int)SoundType.Fire] = "fire";
+            aliases[(int)SoundType.Hit] = "hit";
+        }
+
+        public static void PlaySound(SoundType soundType)
+        {
+            MciSendString( $"open {filePath[(int)soundType]} alias {aliases[(int)soundType]}", null, 0, IntPtr.Zero);
+            // 改变播放位置为开始重新播放
+            if(soundType != SoundType.Start)
+            {
+                MciSendString($"seek {aliases[(int)soundType]} to start", null, 0, IntPtr.Zero);
+            }
+            MciSendString($"play {aliases[(int)soundType]}", null, 0, IntPtr.Zero);
         }
 
         public static void PlayStart()
         {
-            SoundPlayer startPlayer = new SoundPlayer();
-            startPlayer.Stream = Properties.Resources.start;
-            startPlayer.Play();
+            PlaySound(SoundType.Start);
         }
 
         public static void PlayAdd()
         {
-            SoundPlayer addPlayer = new SoundPlayer();
-            addPlayer.Stream = Properties.Resources.add;
-            addPlayer.Play();
+
+            PlaySound(SoundType.Add);
         }
 
         public static void PlayBlast()
         {
-            SoundPlayer blastPlayer = new SoundPlayer();
-            blastPlayer.Stream = Properties.Resources.blast;
-            blastPlayer.Play();
+
+            PlaySound(SoundType.Blast);
         }
 
         public static void PlayFire()
         {
-            SoundPlayer firePlayer = new SoundPlayer();
-            firePlayer.Stream = Properties.Resources.fire;
-            firePlayer.Play();
+
+            PlaySound(SoundType.Fire);
         }
 
         public static void PlayHit()
         {
-            SoundPlayer hitPlayer = new SoundPlayer();
-            hitPlayer.Stream = Properties.Resources.hit;
-            hitPlayer.Play();
+
+            PlaySound(SoundType.Hit);
         }
 
     }
